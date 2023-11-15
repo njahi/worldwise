2ef39ce2d1845f6c02eec7e1400d8a54ea525ae2
Humphrey Gatinu <njahigatinu@gmail.com>
2023-11-15 17:51:46 +0300

    New repository

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import HomePage from "./pages/HomePage";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={<HomePage />}
+        />
+        <Route
+          path='product'
+          element={<Product />}
+        />
+        <Route
+          path='pricing'
+          element={<Pricing />}
+        />
+        <Route
+          path='*'
+          element={<PageNotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,21 @@
+import { NavLink } from "react-router-dom";
+import styles from "./Nav.module.css";
+import Logo from "./Logo";
+export default function Nav() {
+  return (
+    <nav className={styles.nav}>
+      <Logo />
+      <ul>
+        <li>
+          <NavLink to='/pricing'>Pricing</NavLink>
+        </li>
+        <li>
+          <NavLink to='/product'>Product</NavLink>
+        </li>
+        <li>
+          <NavLink to='/login'>Login</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
