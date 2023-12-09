06ba1fd45898253cbe9956c2237ffed7c56c3994
Humphrey Gatinu <njahigatinu@gmail.com>
2023-12-09 15:02:54 +0300

    conditional rendering with nested routes

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,13 +1,14 @@
 import AppNav from "./AppNav";
 import Logo from "./Logo";
 import styles from "./SideBar.module.css";
+import { Outlet } from "react-router-dom";
 
 export default function SideBar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-      <p>List of cities</p>
+      <Outlet />
       <footer className={styles.footer}>
         <p className={styles.copyright}>
           copyright {new Date().getFullYear()}by WorldWise inc.
