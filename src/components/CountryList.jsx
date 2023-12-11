f1df3decd00ab7fa478fc705a4636ae2741fc501
Humphrey Gatinu <njahigatinu@gmail.com>
2023-12-11 16:28:20 +0300

    new message componenet nested into the countryList component

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -2,15 +2,19 @@
 import CountryItem from "./CountryItem";
 import styles from "./CountryList.module.css";
 import Spinner from "./Spinner";
+import Message from "./Message";
 export default function CountryList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
-
+  if (!cities.length) {
+    <Message message='add you first city by clicking on the map' />;
+  }
+  const countries = [];
   return (
     <ul className={styles.countryList}>
-      {cities.map((city) => (
+      {countries.map((country) => (
         <CountryItem
-          key={city.id}
-          city={city}
+          key={country.id}
+          city={country}
         />
       ))}
     </ul>
