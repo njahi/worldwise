cd967d6eb54d9ac6a3afb01aa0b8ca83a2d224e1
Humphrey Gatinu <njahigatinu@gmail.com>
2023-12-11 11:50:01 +0300

    Structuring the cityItem component for reusability

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,5 +1,19 @@
+/* eslint-disable react/prop-types */
 import styles from "./CityItem.module.css";
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(new Date(date));
 
-export default function CityItem() {
-  return <div className={styles.cityItem}>city</div>;
+export default function CityItem({ city }) {
+  const { cityName, emoji, date } = city;
+  return (
+    <li className={styles.cityItem}>
+      <span className={styles.emoji}>{emoji}</span>
+      <h3 className={styles.name}>{cityName}</h3>
+      <time className={styles.date}>{formatDate(date)}</time>
+    </li>
+  );
 }
