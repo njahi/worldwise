eb8c0b8abd6527d68ebb438ab779366d1366b9c2
Humphrey Gatinu <njahigatinu@gmail.com>
2023-12-11 17:23:51 +0300

    helpul comments to our countries variable derived from our reduce method

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -11,8 +11,11 @@ export default function CountryList({ cities, isLoading }) {
     <Message message='add you first city by clicking on the map' />;
   }
   const countries = cities.reduce((arr, city) => {
+    // Check if the current country is not already in the result array
     if (!arr.map((el) => el.country).includes(city.country))
+      // If not, add a new object with country and emoji to the result array
       return [...arr, { country: city.country, emoji: city.emoji }];
+    // If the country is already in the result array, simply return the array as is
     else return arr;
   }, []);
   return (
