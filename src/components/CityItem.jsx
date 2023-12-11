6f1834f37926c4747a2fcff4d36224709a4c741d
Humphrey Gatinu <njahigatinu@gmail.com>
2023-12-11 12:23:57 +0300

    Structuring the cityItem component for reusability

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -14,6 +14,7 @@ export default function CityItem({ city }) {
       <span className={styles.emoji}>{emoji}</span>
       <h3 className={styles.name}>{cityName}</h3>
       <time className={styles.date}>{formatDate(date)}</time>
+      <button className={styles.deleteBtn}></button>
     </li>
   );
 }
