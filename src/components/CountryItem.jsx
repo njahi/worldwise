3fcd5cd865a438ee530f264b48059307e76650f0
Humphrey Gatinu <njahigatinu@gmail.com>
2023-12-11 16:22:47 +0300

    country Item component styling

--- src/components/CountryItem.jsx
+++ src/components/CountryItem.jsx
@@ -4,9 +4,9 @@ import styles from "./CountryItem.module.css";
 export default function CountryItem({ city }) {
   const { emoji, country } = city;
   return (
-    <li>
-      <span className={styles.emoji}>{emoji}</span>
-      <span className={styles.country}>{country}</span>
+    <li className={styles.countryItem}>
+      <span>{emoji}</span>
+      <span>{country}</span>
     </li>
   );
 }
