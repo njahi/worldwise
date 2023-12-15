86b9465baa42b206eb55f0f260596d0647c542f4
Humphrey Gatinu <njahigatinu@gmail.com>
2023-12-15 17:27:31 +0300

    using react router dom parameters to manipulate the UI

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,3 +1,4 @@
+import { useParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 // const formatDate = (date) =>
@@ -9,6 +10,7 @@ import styles from "./City.module.css";
 //   }).format(new Date(date));
 
 function City() {
+  const { id } = useParams();
   // TEMP DATA
   //   const currentCity = {
   //     cityName: "Lisbon",
@@ -18,7 +20,7 @@ function City() {
   //   };
 
   //   const { cityName, emoji, date, notes } = currentCity;
-  return <h1>City</h1>;
+  return <h1>City {id}</h1>;
   //   return (
   //     <div className={styles.city}>
   //       <div className={styles.row}>
