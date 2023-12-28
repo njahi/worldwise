d52173a81188613b43d7ecaee564fa52d6828ebe
Humphrey Gatinu <njahigatinu@gmail.com>
2023-12-28 15:07:56 +0300

    change of server port where data is fetched

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
-const Base_Url = "http://localhost:7000";
+const Base_Url = "http://localhost:9000";
 function App() {
   const [cities, setCities] = useState([]);
   const [isLoading, seTIsLoading] = useState(false);
