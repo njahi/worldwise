2ca7b64a827905c3962e6375f03a01cd70c5c11c
Humphrey Gatinu <njahigatinu@gmail.com>
2023-12-28 16:20:35 +0300

    shairing state details between components via the react router dom custom hook that is the useSearchparams

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,4 +1,16 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 export default function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  // eslint-disable-next-line no-unused-vars
+  const { searchParams, setSearchParams } = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  return (
+    <div className={styles.mapContainer}>
+      <h3>Map</h3>
+      <h1>
+        {lat}, {lng}
+      </h1>
+    </div>
+  );
 }
