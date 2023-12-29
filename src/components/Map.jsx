88d6111f561c883736fc4140474a63db3f302670
Humphrey Gatinu <njahigatinu@gmail.com>
2023-12-29 15:47:55 +0300

    using useNavigate to implement programatic navigation in that on clicking the map component takes the user to the form route nested in the app route

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,15 +1,26 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 export default function Map() {
+  const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
   return (
-    <div className={styles.mapContainer}>
+    <div
+      className={styles.mapContainer}
+      onClick={() => {
+        navigate("form");
+      }}>
       <h3>Map</h3>
       <h1>
         position:{lat}, {lng}
       </h1>
+      <button
+        onClick={() => {
+          setSearchParams({ lat: 30, lng: 50 });
+        }}>
+        change position
+      </button>
     </div>
   );
 }
