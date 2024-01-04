68f409ac81fcc671d8f1d85138598064cdfaa0d6
Humphrey Gatinu <njahigatinu@gmail.com>
2024-01-04 14:28:35 +0300

    change the tile-layer component style

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -17,7 +17,7 @@ export default function Map() {
         className={styles.map}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
         />
         <Marker position={mapPosition}>
           <Popup>
