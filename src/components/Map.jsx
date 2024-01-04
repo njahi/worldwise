1224c2056f6529ac3a5b4962c242f18cca90ad08
Humphrey Gatinu <njahigatinu@gmail.com>
2024-01-04 15:24:35 +0300

    enable screen zoom on map when srolled

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -15,7 +15,7 @@ export default function Map() {
       <MapContainer
         center={mapPosition}
         zoom={13}
-        scrollWheelZoom={false}
+        scrollWheelZoom={true}
         className={styles.map}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -34,4 +34,3 @@ export default function Map() {
     </div>
   );
 }
-maped each city by id giving them each a marker from the react ui library components from the react-leaflet package
