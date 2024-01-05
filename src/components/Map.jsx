5d8da6001799401f9eae40c0ca4a82f4f07692f8
Humphrey Gatinu <njahigatinu@gmail.com>
2024-01-05 20:19:02 +0300

    setting the map to change marker position on change of the lat&lng states stored in the url

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,18 +1,34 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 import { useNavigate, useSearchParams } from "react-router-dom";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvent,
+} from "react-leaflet";
 import styles from "./Map.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useCities } from "../context/CityContext";
 export default function Map() {
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
+  // setting the map to change marker position using the url query strings(lat && lng)
+  // useEffect(
+  //   function () {
+  //     if (lat && lng) setMapPosition([lat, lng]);
+  //   },
+  //   [lat, lng]
+  // );
   return (
     <div className={styles.mapContainer}>
       <MapContainer
-        center={[lat, lng]}
+        center={mapPosition}
         zoom={6}
         scrollWheelZoom={true}
         className={styles.map}>
@@ -30,6 +46,8 @@ export default function Map() {
             </Popup>
           </Marker>
         ))}
+        {/* <ChangeCenter position={mapPosition} />
+        <DetectClick /> */}
       </MapContainer>
     </div>
   );
