d8790fa8c77d1e4606d9ff1a5fad5f4c9bd57b46
Humphrey Gatinu <njahigatinu@gmail.com>
2024-01-08 17:27:30 +0300

    change of params set in the form url path via the map custom hooksin the map component

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -79,7 +79,8 @@ function ChangeCenter({ position }) {
 }
 function DetectClick() {
   const navigate = useNavigate();
+
   useMapEvent({
-    click: (e) => navigate(`form?llat=${e.latlng.lat} & lng=${e.latlng.lng} `),
+    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng} `),
   });
 }
