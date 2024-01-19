6b14454282ce300d662a3f1035906eb075c989ca
Humphrey Gatinu <njahigatinu@gmail.com>
2024-01-19 17:36:35 +0300

    changed the redirection page once the link on the Homepage is clicked

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -19,7 +19,7 @@ export default function Homepage() {
           you have wandered the world.
         </h2>
         <Link
-          to='/app'
+          to='/login'
           className='cta'>
           Start tracking now
         </Link>
