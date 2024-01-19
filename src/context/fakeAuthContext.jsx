4638e080edaac264e9b2e0eab0cfb5f03a8b272d
Humphrey Gatinu <njahigatinu@gmail.com>
2024-01-19 13:13:51 +0300

    added the login and logout logic in the fakeauthcontext api

--- src/context/fakeAuthContext.jsx
+++ src/context/fakeAuthContext.jsx
@@ -8,6 +8,12 @@ const initialState = {
   user: null,
   isAuthenticated: false,
 };
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
 function reducer(state, action) {
   switch (action.type) {
     case "login":
@@ -24,8 +30,14 @@ function AuthProvider({ children }) {
     reducer,
     initialState
   );
-  function login(email, password) {}
-  function logout() {}
+  function login(email, password) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password) {
+      dispatch({ type: "login", payload: FAKE_USER });
+    }
+  }
+  function logout() {
+    dispatch({ type: "logout" });
+  }
   return (
     <AuthContext.Provider
       value={{
