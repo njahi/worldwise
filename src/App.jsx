a67e3dc5f22af6710c95d5bbba8c1a3d56e2f2b8
Humphrey Gatinu <njahigatinu@gmail.com>
2024-02-21 12:18:47 +0300

    new component that renders a spinner that spins on the whole page before rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { CitiesProvider } from "./context/CityContext";
 import { AuthProvider } from "./context/fakeAuthContext";
+import { lazy } from "react";
 import ProtectedRoute from "./pages/ProtectedRoute";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
-import { lazy } from "react";
+
 const HomePage = lazy(() => import("./pages/HomePage"));
 const Product = lazy(() => import("./pages/Product"));
 const Pricing = lazy(() => import("./pages/Pricing"));
 const AppLayout = lazy(() => import("./pages/AppLayout"));
 const Login = lazy(() => import("./pages/Login"));
 const PageNotFound = lazy(() => import("./pages/PageNotFound"));
-// import Product from "./pages/Product";
-// import HomePage from "./pages/HomePage";
-// import Pricing from "./pages/Pricing";
-// import PageNotFound from "./pages/PageNotFound";
-// import AppLayout from "./pages/AppLayout";
-// import Login from "./pages/Login";
 
 function App() {
   return (
